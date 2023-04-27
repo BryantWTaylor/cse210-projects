9d1e97072f15c27bfe10a7d16fb6162b558eb476
BryantWTaylor <bryant.taylor12@gmail.com>
2023-04-26 21:54:53 -0600

    Prep 3 assignment completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 101);
+        int convGuess = -1;
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            convGuess = int.Parse(userGuess);
+            if (mNumber == convGuess) 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (convGuess > mNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (convGuess != mNumber);    
     }
 }
