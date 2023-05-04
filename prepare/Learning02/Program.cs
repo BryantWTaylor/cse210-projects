071af825f2a6f9104dbb093d6a2d9c19b5eb8be9
BryantWTaylor <bryant.taylor12@gmail.com>
2023-05-03 23:06:04 -0600

    week 3 learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Sales Representative";
+        job1._company = "Wireless Advocates";
+        job1._startYear = 2017;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Optician";
+        job2._company = "Costco";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Bryant Taylor";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
